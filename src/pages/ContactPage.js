2dcc67ea48a498cf36ece237975779b4d34050b9
adarsh3699 <adarsh3699@gmail.com>
2022-05-17 13:17:52 +0530

    msg send

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Bar from "../components/Bar";
 import FootBar from "../components/FootBar";
-
 import "../css/contact.css"
-import "https://s.pageclip.co/v1/pageclip.css"
 
 import emailIconImg from "../img/email.png"
 import telegramIconImg from "../img/telegram.svg"
@@ -12,13 +10,19 @@ import youtubeIconImg from "../img/youtube2.png"
 import websiteIconImg from "../img/website.png"
 
 function AboutPage() {
+    const [nameValue, setNameValue] = useState("");
     const [emailValue, setEmailValue] = useState("");
     const [msgValue, setMsgValue] = useState("");
-    const [error, setError] = useState({});
 
     useEffect(() => {
         document.title = "Adarsh Suman | Contact"
-     }, []);
+    }, []);
+
+    function handelNameChange(e) {
+        const value = e.target.value;
+        setNameValue(value)
+    }
+
 
     function handelEmailChange(e) {
         const value = e.target.value;
@@ -37,13 +41,17 @@ function AboutPage() {
             <div id="background">
 
                 <div id="title">Contact Us</div>
-                <form id="msgUs" className="pageclip-form" method="post" action="https://send.pageclip.co/YYFiRgfPYOw99mcNfB9R8RHgiwcappSO">
+                <form id="msgUs" method="post" action="https://send.pageclip.co/YYFiRgfPYOw99mcNfB9R8RHgiwcappSO">
                     <div>
-                        <input type="email" placeholder="Email" required name="email" id="email" value={emailValue} onChange={handelEmailChange}/>
+                        <input type="text" className='invalid' placeholder="Enter Your Name" required name="name" id="email" value={nameValue} onChange={handelEmailChange}  />
                     </div>
 
                     <div>
-                        <textarea id="textArea" required name="body" cols="40" rows="13" placeholder="Write your msg" value={msgValue} onChange={handelMsgChange}></textarea>
+                        <input type="email" className='invalid' placeholder=" Enter Your Email" required name="email" id="email" value={emailValue} onChange={handelEmailChange} />
+                    </div>
+
+                    <div>
+                        <textarea id="textArea" required name="body" cols="40" rows="13" placeholder="Write your msg here" value={msgValue} onChange={handelMsgChange}></textarea>
                     </div>
                     <div>
                         <button id="send" className="pageclip-form__submit"><span>Send</span></button>
@@ -88,7 +96,7 @@ function AboutPage() {
 
                     <div>
                         <a href="https://github.com/adarsh3699" target="_blank">
-                            <svg height="30"  viewBox="0 0 16 16" version="1.1" id="git">
+                            <svg height="30" viewBox="0 0 16 16" version="1.1" id="git">
                                 <path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
                             </svg>
                             <div>GitHub</div>
